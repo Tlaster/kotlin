85f1529d3702c00804a5dacef049baab9ce3f8ab
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2022-07-22 12:11:55 +0300

    fixup! [K/N] Add option to omit generation of binary when producing framework

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/KonanConfig.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/KonanConfig.kt
@@ -403,12 +403,12 @@ class KonanConfig(val project: Project, val configuration: CompilerConfiguration
      * This is useful when user care only about framework's interface.
      */
     internal val omitFrameworkBinary: Boolean by lazy {
-        val enabled = configuration.getBoolean(KonanConfigKeys.OMIT_FRAMEWORK_BINARY)
-        if (enabled && produce != CompilerOutputKind.FRAMEWORK) {
-            configuration.report(CompilerMessageSeverity.STRONG_WARNING,
-                    "Trying to disable framework binary compilation when producing ${produce.name.lowercase()} is meaningless.")
+        configuration.getBoolean(KonanConfigKeys.OMIT_FRAMEWORK_BINARY).also {
+            if (it && produce != CompilerOutputKind.FRAMEWORK) {
+                configuration.report(CompilerMessageSeverity.STRONG_WARNING,
+                        "Trying to disable framework binary compilation when producing ${produce.name.lowercase()} is meaningless.")
+            }
         }
-        enabled
     }
 }
 
